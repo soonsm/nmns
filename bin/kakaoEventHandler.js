1f0da9972efd523abb9b5fdd869debc6dfc999ad
soonsm <soonsm@gmail.com>
2018-04-12 23:23:59 +0900

    예약완료 알림톡 보내기까지 함

--- bin/kakaoEventHandler.js
+++ bin/kakaoEventHandler.js
@@ -10,7 +10,11 @@ const
     apiStoreKey = 'Nzc4MS0xNTIwNDI3MTgxMzc4LTMyMTQ4M2I1LTBiODUtNDYxNC05NDgzLWI1MGI4NWY2MTQzNw==';
 
 function formatPhone(phone){
-    return phone.length === 11 ? (phone.substring(0,3) + '-' + phone.substring(3, 7) + '-' + phone.substring(7)) : (phone.substring(0,3) + '-' + phone.substring(3, 6) + '-' + phone.substring(6));
+    try{
+        return phone.length === 11 ? (phone.substring(0,3) + '-' + phone.substring(3, 7) + '-' + phone.substring(7)) : (phone.substring(0,3) + '-' + phone.substring(3, 6) + '-' + phone.substring(6));
+    }catch(e){
+        return '';
+    }
 }
 
 function phoneNumberValidation(phone){
@@ -22,67 +26,102 @@ function phoneNumberValidation(phone){
 /*
 TODO List
 1. 노쇼 구분해서 보여주기(우리샵 노쇼, 다른샵 노쇼)
+ 저장할 때: KaKaoUserList에 내 노쇼 리스트 따로 저장 후 NoShowList에 추가
+ 조회할 때: KaKaoUserList에 내 노쇼 리스트 조회 + NoShowList 조회
 2. 노쇼 등록 취소하기(마지막으로 등록한 노쇼취소, 전화번호 입력받아서 취소(단, 내가 등록한 노쇼에 한하여))
+ 저장할 때: 마지막 저장한 노쇼 컬럼에 해당 노쇼키 저장
+ 취소할 때: 마지막 등록한 노쇼 취소 일 때는 KaKaoUserList에서 NoShowKey 갖고와서 지운다.(내 리스트, NoShowList)
+          전화번호 입력시에는 내 노쇼 리스트에 있으면 내 리스트와 전체 리스트에서 지우고, 아니면 에러 리턴
  */
 
-async function sendReservationCancelNotify(user, alrimTalk){
-
-    let msg = `[${user.shopName} 예약취소알림]\n예약 취소를 알려드립니다.\n예약날짜: ${moment(alrimTalk.reservationDate, 'MMDD').format('MM[월]DD[일]')}\n예약시간: ${moment(alrimTalk.reservationTime,'HHmm').format('HH[시]mm[분]')} \n예약자 전화번호: ${formatPhone(alrimTalk.receiverPhone)}`;
+async function sendAlrimTalk(param){
     let result = false;
 
     await request({
         "uri": `http://api.apistore.co.kr/kko/1/msg/${apiStoreId}`,
         "headers": {'x-waple-authorization': apiStoreKey},
         "method": "POST",
-        "json": {
-            phone: user.userPhone,
-            callback: user.userPhone,
-            reqdate: moment().format('YYYYMMDDHHmmss'),
-            msg: msg,
-            template_code: 'A002',
-        }
+        'form' : param
     }, (err, res, body) => {
         if (!err) {
-            console.log('ReservationCancelNotify sent!: ', body)
+            console.log('sendAlrimTalk sending result: ', body)
             if(body.result_code === '200'){
                 result = true;
             }
         } else {
-            console.error("Unable to send ReservationCancelNotify:", body);
+            console.error("Unable to send sendAlrimTalk:", body);
         }
     });
 
     return result;
 }
 
+async function sendReservationCancelNotify(user, alrimTalk){
 
-async function sendAlrimTalk(user, alrimTalk) {
+    let msg = `[${user.shopName} 예약취소알림]\n예약 취소를 알려드립니다.\n예약날짜: ${moment(alrimTalk.reservationDate, 'MMDD').format('MM[월]DD[일]')}\n예약시간: ${moment(alrimTalk.reservationTime,'HHmm').format('HH[시]mm[분]')} \n예약자 전화번호: ${formatPhone(alrimTalk.receiverPhone)}`;
+
+    return await sendAlrimTalk({
+        phone: user.userPhone,
+        callback: user.userPhone,
+        reqdate: moment().format('YYYYMMDDHHmmss'),
+        msg: msg,
+        template_code: 'A002'
+    });
+}
+
+
+async function sendReservationConfirm(user, alrimTalk) {
 
     let msg = `[${user.shopName} 예약안내]\n예약날짜: ${moment(alrimTalk.reservationDate, 'MMDD').format('MM[월]DD[일]')}\n예약시간: ${moment(alrimTalk.reservationTime,'HHmm').format('HH[시]mm[분]')}\n방문시준비사항: ${user.messageWithConfirm}\n- 예약취소는 ${user.cancelDue}전까지 가능합니다.\n- 예약취소를 원하실 때는 꼭 예약취소 버튼을 눌러주시기 바랍니다.`;
-    let result = false;
 
+    return await sendAlrimTalk({
+        phone: alrimTalk.receiverPhone,
+        callback: '01028904311',
+        msg: msg,
+        template_code: 'A003',
+        url: `http://ec2-13-125-29-64.ap-northeast-2.compute.amazonaws.com/cancel/key=${alrimTalk.reservationKey}`,
+        url_button_txt: '예약취소',
+        apiVersion: 1,
+        client_id: apiStoreId
+    });
+}
+
+async function sendSenderRegister(){
     await request({
-        "uri": `http://api.apistore.co.kr/kko/1/msg/${apiStoreId}`,
+        "uri": `http://api.apistore.co.kr/kko/2/sendnumber/save/${apiStoreId}`,
         "headers": {'x-waple-authorization': apiStoreKey},
         "method": "POST",
-        "json": {
-            phone: alrimTalk.receiverPhone,
-            callback: user.userPhone,
-            reqdate: moment().format('YYYYMMDDHHmmss'),
-            msg: msg,
-            template_code: 'A003',
-            url: `http://ec2-13-125-29-64.ap-northeast-2.compute.amazonaws.com/cancel/key=${alrimTalk.reservationKey}`,
-            url_button_txt: '예약취소'
+        'form' : {
+            sendnumber : '01028904311',
+            comment: 'NoMoreNoShow대표번호',
+            pintype: 'SMS',
+            pincode: '693181'
         }
     }, (err, res, body) => {
         if (!err) {
-            console.log('ReservationCancelNotify sent!: ', body)
+            console.log('발신번호 인증/등록 전송 성공: ', body)
             if(body.result_code === '200'){
-                result = true;
+                return true;
             }
         } else {
-            console.error("Unable to send message:" + body);
+            console.error("발신번호 인증/등록 전송 실패:", body);
         }
+    });
+}
+
+async function sendTestAlrimTalk() {
+
+    let msg = `[승민네일샵 예약안내]\n예약날짜: 4월5일\n예약시간: 17시30분\n방문시준비사항: 씻고와라\n- 예약취소는 하루전까지 가능합니다.\n- 예약취소를 원하실 때는 꼭 예약취소 버튼을 눌러주시기 바랍니다.`;
+
+    return await sendAlrimTalk({
+        phone: '01028904311',
+        callback: '01028904311',
+        msg: msg,
+        template_code: 'A003',
+        url: `http://203.128.198.113:8088/cancel/key=20180328111018.686DBxu7bqxT_q25e9fcdbe3812c9c8d768e4ba2e6b52eebde96e89be3266278f31feef91065263`,
+        url_button_txt: '예약취소',
+        apiVersion: 1,
+        client_id: apiStoreId
     });
 }
 
@@ -103,6 +142,10 @@ exports.messageHandler = async function(userKey, content, res){
         let user = await db.getUser(userKey);
         if(user){
             if(user.hasRightToSendConfirm){
+                //TODO: TEST
+                // await sendSenderRegister();
+                // await sendTestAlrimTalk();
+                // returnMessage = message.messageWithHomeKeyboard('전송되었습니다.');
                 await db.setUserStatus(userKey, userStatus.beforeTypeAlrimTalkInfo, 'sendConfirmTryCount');
                 returnMessage = message.typeAlrimTalkInfo();
             }else{
@@ -119,7 +162,7 @@ exports.messageHandler = async function(userKey, content, res){
                 if(user.onGoingAlrimTalkKey){
                     let alrimTalk = await db.getAlrimTalk(user.onGoingAlrimTalkKey);
                     alrimTalk.isConfirmed = true;
-                    if(await sendAlrimTalk(user,alrimTalk)){
+                    if(await sendReservationConfirm(user,alrimTalk)){
                         alrimTalk.isSent = true;
                         await db.saveAlrimTalk(alrimTalk);
                         //사용자 정보에 전송 횟수 업데이트
@@ -155,7 +198,7 @@ exports.messageHandler = async function(userKey, content, res){
                     break;
                 case userStatus.beforeRetrieve:
                     let noShow = await db.getNoShow(phoneNumber);
-                    if(noShow){
+                    if(noShow && noShow.noShowCount > 0){
                         const lastNoShowDate = noShow.lastNoShowDate;
                         const lastNoShowDateStr = `${lastNoShowDate.substring(0,4)}년 ${lastNoShowDate.substring(4,6)}월 ${lastNoShowDate.substring(6)}일`;
                         returnMessage = message.messageWithHomeKeyboard(`입력하신 번호는 NoShow 전적이 ${noShow.noShowCount}번 있습니다.\n(마지막 NoShow: ${lastNoShowDateStr})`);
