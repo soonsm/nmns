bbf547f554456927c13d1ca1748f086b056660ed
iKoru <glassonyou@gmail.com>
2018-08-19 21:38:11 +0900

    bootstrap notify 닫기버튼 위치 조정, 이용약관 동의 의무화, 팁 보여줄 확률 70% 적용, password autocomplete 제거

--- client/static/nmns/js/main.js
+++ client/static/nmns/js/main.js
@@ -254,6 +254,11 @@
         body:"인증메일은 내 매장 정보 화면에서 다시 보내실 수 있습니다. 이메일을 인증해주세요!"
       });
     }
+    if(NMNS.info.isFirstVisit || ((getCookie("showTips") === "true" || getCookie("showTips") === undefined) && Math.random() < 0.7)){
+      $("#tipsModal").modal("show");
+    } else if (getCookie("showTips") !== "true") {
+      $("#showTips").removeClass("d-none");
+    }
   }));
   
   NMNS.socket.on("get manager", socketResponse("매니저 정보 받아오기", function(e){
@@ -1453,11 +1458,6 @@
   setDropdownCalendarType();
   setRenderRangeText();
   setEventListener();
-  if(getCookie("showTips") === "true" || getCookie("showTips") === undefined){
-    $("#tipsModal").modal("show");
-  } else {
-    $("#showTips").removeClass("d-none");
-  }
 //after calendar initialization end
 //websocket response start
   NMNS.socket.on("get summary", socketResponse("예약정보 가져오기", function(e){
