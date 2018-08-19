5bd65b13ee636cee752aea37f689df68b03f737b
Ubuntu <ubuntu@ip-172-31-28-53.ap-northeast-2.compute.internal>
2018-08-19 11:30:37 +0000

    ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,8 +9,8 @@ module.exports = {
             // pm2로 실행될 파일 경로
             script: "./app.js",
             // 배포환경시 적용될 설정 지정
-            env_production: {
-                "PORT": 8080,
+            env: {
+                "PORT": 8088,
                 "NODE_ENV": "production"
             }
         }
