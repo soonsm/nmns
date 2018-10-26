f1afee996e29248d09352afe62a6d1aaac1c5a0e
koru <glassonyou@gmail.com>
2018-10-26 07:04:19 +0000

    debounce 함수 추가

--- client/static/nmns/js/util.js
+++ client/static/nmns/js/util.js
@@ -352,3 +352,16 @@ function validateEmail(email)
 
 	return true;
 }
+
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		clearTimeout(timeout);
+		timeout = setTimeout(function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		}, wait);
+		if (immediate && !timeout) func.apply(context, args);
+	};
+}
