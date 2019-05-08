4f47be071adc4e428064ef13b37078507c252b48
soonsm <soonsm@gmail.com>
2019-05-09 03:25:58 +0900

    Table 설정 관련 코드 추가

---
+++ local_db/statistics.js
@@ -0,0 +1,66 @@
+
+'use strict';
+var AWS = require("aws-sdk");
+AWS.config.update({
+    region: "ap-northeast-2"
+});
+
+var docClient = new AWS.DynamoDB.DocumentClient({
+    convertEmptyValues: true
+});
+function scan(params) {
+    return new Promise((resolve => {
+        docClient.scan(params, function (err, data) {
+            if (err) {
+                resolve(null);
+            } else {
+                resolve(data.Items);
+            }
+        });
+    }));
+}
+
+// (async function(){
+//     let users = await scan({
+//         TableName: 'WebSecheduler'
+//     });
+//     let i=0;
+//     for (; i < users.length; i++) {
+//         let user = users[i];
+//         if(user.authStatus === 'EMAIL_VERIFICATED' && user.visitLog){
+//             let visitLog = user.visitLog;
+//             let lastVisit = visitLog[visitLog.length - 1];
+//             let rLength = user.reservationList.length;
+//
+//             if((lastVisit > '20190401' && rLength > 0) || rLength > 50){
+//                 console.log(`${user.email} ${lastVisit} ${rLength}`);
+//             }
+//
+//         }
+//         // if(user.memberList){
+//         //     user.memberList.forEach(member => {
+//         //         if(member.etc){
+//         //             console.log(member.etc);
+//         //         }
+//         //     });
+//         // }
+//     }
+//     console.log(`total count is ${i}`);
+// })();
+
+(async function(){
+    let users = await scan({
+        TableName: 'KaKaoUserList'
+    });
+    let count=0;
+    let count2=0
+    users.forEach(user => {
+        if(user.email){
+            count++;
+        }
+        if(user.lastVisitDate > '20190505'){
+            count2++;
+        }
+    })
+    console.log(`total email count is ${count} and recent visit user is ${count2}`);
+})();
