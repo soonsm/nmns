5e34bd5866f75a155eceb694d57ffe8d27575be4
soonsm <soonsm@gmail.com>
2019-07-13 13:43:13 +0900

    email background color change

--- bin/emailSender.js
+++ bin/emailSender.js
@@ -68,11 +68,11 @@ const getEmailVerificaitonText = function(param){
     return '\n' +
         '<div style="margin:0;padding:0">\n' +
         '<link href="https://fonts.googleapis.com/css?family=Montserrat:700|Nanum+Gothic" rel="stylesheet">\n' +
-        '<div style="max-width:100%;padding:15px;background-color:#f0f0f0;box-sizing:border-box">\n' +
+        '<div style="max-width:100%;padding:15px;background-color:#303F4F;box-sizing:border-box">\n' +
         '<table style="margin:0 auto;padding:0;width:100%;max-width:630px;font-family:\'NanumGothic\',dotum" cellspacing="0" cellpadding="0">\n' +
         '<tbody>\n' +
         '<tr style="background-color:#009688;height:50px">\n' +
-        '<td style="vertical-align:middle;padding-left:10px;padding-top:5px;"><a href="https://www.nomorenoshow.co.kr" style="font-family:\'Montserrat\', \'sans-serif\'; font-weight:700;height:40px;display:inline-block;color:white;font-size:1.8rem;text-decoration:none;" >No More No Show</a></td>\n' +
+        '<td style="vertical-align:middle;padding-left:10px;padding-top:5px;"><a href="https://www.washow.co.kr" style="font-family:\'Montserrat\', \'sans-serif\'; font-weight:700;height:40px;display:inline-block;color:white;font-size:1.8rem;text-decoration:none;" >WA:SHOW</a></td>\n' +
         '</tr>\n' +
         '<tr>\n' +
         '<td style="padding:50px 20px;background:#fff">\n' +
@@ -82,21 +82,21 @@ const getEmailVerificaitonText = function(param){
         '<td style="padding:27px 0 17px;font-size:30px;line-height:34px;color:#333"><strong style="color:#009688;font-weight:normal">이메일 계정 인증</strong>안내</td>\n' +
         '</tr>\n' +
         '<tr>\n' +
-        '<td style="padding:10px 0 30px;font-size:15px;line-height:27px;color:#303030">안녕하세요. No More No Show입니다.<br>\n' +
-        '항상 No More No Show를 사랑해 주시는 고객님께 진심으로 감사 드립니다.<br>\n' +
+        '<td style="padding:10px 0 30px;font-size:15px;line-height:27px;color:#303030">안녕하세요. WA:SHOW입니다.<br>\n' +
+        '항상 WA:SHOW를 사랑해 주시는 고객님께 진심으로 감사 드립니다.<br>\n' +
         '<br>\n' +
         '본 메일은 고객님의 이메일 계정 인증을 위하여 발송된 것입니다.<br>\n' +
         '아래의 버튼을 누르시면 인증이 완료됩니다.\n' +
         '</tr>\n' +
         '<tr>\n' +
         '<td style="padding:30px 0 0;border-top:1px solid #e0e0e0">\n' +
-        '<a href="https://www.nomorenoshow.co.kr/' + param + '" style="border-radius:none;border:1px solid #009688;cursor:pointer;color:#fff;text-align:center;font-size:1.2rem;display:inline-block;background-color:#009688;font-family:\'NanumGothic\';font-weight:normal;text-decoration:none; padding:12px 30px;"><span style="height:40px;">인증하기</span></a>\n' +
+        '<a href="https://www.washow.co.kr/' + param + '" style="border-radius:none;border:1px solid #009688;cursor:pointer;color:#fff;text-align:center;font-size:1.2rem;display:inline-block;background-color:#009688;font-family:\'NanumGothic\';font-weight:normal;text-decoration:none; padding:12px 30px;"><span style="height:40px;">인증하기</span></a>\n' +
         '</td>\n' +
         '</tr>\n' +
         '<tr>\n' +
         '<td style="padding-top:30px;font-size:15px;line-height:27px;color:#303030">감사합니다.<br>\n' +
         '<br>\n' +
-        '<strong style="color:#333">No More No Show 드림</strong>\n' +
+        '<strong style="color:#333">WA:SHOW 드림</strong>\n' +
         '</td>\n' +
         '</tr>\n' +
         '</tbody>\n' +
@@ -104,14 +104,14 @@ const getEmailVerificaitonText = function(param){
         '</td>\n' +
         '</tr>\n' +
         '<tr>\n' +
-        '<td style="padding:25px 0;background:#f0f0f0">\n' +
+        '<td style="padding:25px 0;background:#303F4F">\n' +
         '<table style="margin:0;padding:0;width:100%" cellspacing="0" cellpadding="0">\n' +
         '<tbody>\n' +
         '<tr>\n' +
         '<td style="font-size:13px;line-height:20px;color:#666">\n' +
-        '<a href="https://www.nomorenoshow.co.kr">No More No Show</a><br>\n' +
-        '이메일 : <a href="mailto:support@nomorenoshow.co.kr" style="color:#666;text-decoration:none" target="_blank">support@nomorenoshow.co.kr</a><br>\n' +
-        '©No More No Show</td>\n' +
+        '<a href="https://www.washow.co.kr">WA:SHOW</a><br>\n' +
+        '이메일 : <a href="mailto:support@washow.co.kr" style="color:#666;text-decoration:none" target="_blank">support@washow.co.kr</a><br>\n' +
+        '©WA:SHOW</td>\n' +
         '</tr>\n' +
         '</tbody>\n' +
         '</table>\n' +
@@ -127,11 +127,11 @@ const getPasswordResetText = function(newPassword){
     return '\n' +
         '<div style="margin:0;padding:0">\n' +
         '<link href="https://fonts.googleapis.com/css?family=Montserrat:700|Nanum+Gothic" rel="stylesheet">\n' +
-        '<div style="max-width:100%;padding:15px;background-color:#f0f0f0;box-sizing:border-box">\n' +
+        '<div style="max-width:100%;padding:15px;background-color:#303F4F;box-sizing:border-box">\n' +
         '<table style="margin:0 auto;padding:0;width:100%;max-width:630px;font-family:\'NanumGothic\',dotum" cellspacing="0" cellpadding="0">\n' +
         '<tbody>\n' +
         '<tr style="background-color:#009688;height:50px">\n' +
-        '<td style="vertical-align:middle;padding-left:10px;padding-top:5px;"><a href="https://www.nomorenoshow.co.kr" style="font-family:\'Montserrat\', \'sans-serif\'; font-weight:700;height:40px;display:inline-block;color:white;font-size:1.8rem;text-decoration:none;" >No More No Show</a></td>\n' +
+        '<td style="vertical-align:middle;padding-left:10px;padding-top:5px;"><a href="https://www.washow.co.kr" style="font-family:\'Montserrat\', \'sans-serif\'; font-weight:700;height:40px;display:inline-block;color:white;font-size:1.8rem;text-decoration:none;" >WA:SHOW</a></td>\n' +
         '</tr>\n' +
         '<tr>\n' +
         '<td style="padding:50px 20px;background:#fff">\n' +
@@ -141,8 +141,8 @@ const getPasswordResetText = function(newPassword){
         '<td style="padding:27px 0 17px;font-size:30px;line-height:34px;color:#333"><strong style="color:#009688;font-weight:normal">임시비밀번호 발급</strong>안내</td>\n' +
         '</tr>\n' +
         '<tr>\n' +
-        '<td style="padding:10px 0 30px;font-size:15px;line-height:27px;color:#303030">안녕하세요. No More No Show입니다.<br>\n' +
-        '항상 No More No Show를 사랑해 주시는 고객님께 진심으로 감사 드립니다.<br>\n' +
+        '<td style="padding:10px 0 30px;font-size:15px;line-height:27px;color:#303030">안녕하세요. WA:SHOW입니다.<br>\n' +
+        '항상 WA:SHOW를 사랑해 주시는 고객님께 진심으로 감사 드립니다.<br>\n' +
         '<br>\n' +
         '본 메일은 고객님의 임시 비밀번호 발급을 위하여 발송된 것입니다.<br>\n' +
         '임시 비밀번호는 아래와 같습니다.\n' +
@@ -155,7 +155,7 @@ const getPasswordResetText = function(newPassword){
         '<tr>\n' +
         '<td style="padding-top:30px;font-size:15px;line-height:27px;color:#303030">감사합니다.<br>\n' +
         '<br>\n' +
-        '<strong style="color:#333">No More No Show 드림</strong>\n' +
+        '<strong style="color:#333">WA:SHOW 드림</strong>\n' +
         '</td>\n' +
         '</tr>\n' +
         '</tbody>\n' +
@@ -163,14 +163,14 @@ const getPasswordResetText = function(newPassword){
         '</td>\n' +
         '</tr>\n' +
         '<tr>\n' +
-        '<td style="padding:25px 0;background:#f0f0f0">\n' +
+        '<td style="padding:25px 0;background:#303F4F">\n' +
         '<table style="margin:0;padding:0;width:100%" cellspacing="0" cellpadding="0">\n' +
         '<tbody>\n' +
         '<tr>\n' +
         '<td style="font-size:13px;line-height:20px;color:#666">\n' +
-        '<a href="https://www.nomorenoshow.co.kr">No More No Show</a><br>\n' +
-        '이메일 : <a href="mailto:support@nomorenoshow.co.kr" style="color:#666;text-decoration:none" target="_blank">support@nomorenoshow.co.kr</a><br>\n' +
-        '©No More No Show</td>\n' +
+        '<a href="https://www.washow.co.kr">WA:SHOW</a><br>\n' +
+        '이메일 : <a href="mailto:support@washow.co.kr" style="color:#666;text-decoration:none" target="_blank">support@washow.co.kr</a><br>\n' +
+        '©WA:SHOW</td>\n' +
         '</tr>\n' +
         '</tbody>\n' +
         '</table>\n' +
